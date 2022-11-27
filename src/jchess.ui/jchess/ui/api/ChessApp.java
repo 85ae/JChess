e751dc3b49b89a089db444c92182b6f0f40f26a9
85ae <jolan.oubrier@gmail.com>
2022-11-27 17:39:59 +0000

    Created jchess.ui/jchess.ui.api.ChessApp class.

---
+++ src/jchess.ui/jchess/ui/api/ChessApp.java
@@ -0,0 +1,11 @@
+package jchess.ui.api;
+
+/** 
+ * This class is a chess application.<br/>
+ * You can use it in a application to create the interface.
+ * It works in both console and graphical.<br/>
+ * The best way to use it is to create an inherited class and define the methods by yourself.
+ */
+public class ChessApp {
+    
+}
--- src/jchess.ui/module-info.java
+++ src/jchess.ui/module-info.java
@@ -1,6 +1,6 @@
 /** A command-line interface to play chess.
  * You can use it to display chess games, play chess...
- * The API is situated in the package jchess.interface.[TODO].
+ * The API is situated in the package jchess.interface.
  */
 module jchess.ui {
     requires java.base;
